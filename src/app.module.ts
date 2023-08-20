ed618d7a3a57ffdc61b54f93f41a8fbabd96164a
Cristian Franco <cfrancobedoya@gmail.com>
2023-08-20 00:01:29 -0500

    [CI-CD] Add Mongo DB connection as inyectable

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,24 +6,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-import { MongoClient } from 'mongodb';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import config from './config';
 import { enviroments } from './enviroments';
-
-const uri =
-  'mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary';
-
-const client = new MongoClient(uri);
-async function run() {
-  await client.connect();
-  const database = client.db('my-store');
-  const taskCollection = database.collection('tasks');
-  const tasks = await taskCollection.find().toArray();
-  console.log(tasks);
-}
-run();
 
 @Module({
   imports: [
